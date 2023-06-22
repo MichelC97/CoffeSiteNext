0d038fdaa02282de78b7eaf1136da84f16559f9b
Michel Carretto <Michel.Alumno@gmail.com>
2023-06-22 17:02:39 -0300

    document.tsx type fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,11 @@
-import Document from "next/document";
+import React from "react";
+import Document, { DocumentInitialProps, DocumentContext } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
@@ -16,7 +19,12 @@ export default class MyDocument extends Document {
       const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
-        styles: [initialProps.styles, sheet.getStyleElement()],
+        styles: [
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>,
+        ],
       };
     } finally {
       sheet.seal();
